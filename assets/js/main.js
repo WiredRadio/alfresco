644791d3b83193b995754971cd7500c508c77cd4
Hugh Rawlinson <hugh@z.(none)>
2015-02-08 18:44:51 +0000

    Initial Commit

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+var stream = null;
+
+var togglePlayback = function(){
+	stream.paused?stream.play():stream.pause();
+	$($('#playPause').find('i')).attr('class',stream.paused?'fa fa-play':'fa fa-pause');
+}
+
+soundManager.setup({
+    url: '/assets/js/vendor/swf',
+    onready: function() {
+        stream = soundManager.createSound({
+            id: 'stream',
+            url: 'http://54.213.70.47:8000/wired'
+        });
+        stream.play();
+        $('#loading').hide();
+    },
+    ontimeout: function() {
+    // Hrmm, SM2 could not start. Missing SWF? Flash blocked? Show an error, etc.?
+    }
+});
